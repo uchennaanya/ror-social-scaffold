a76e207b11b8e607b865fdeaa24576f48097e3b5
Chukwuma Obasi <ccobasi8@gmail.com>
2021-04-27 16:04:09 +0100

    Add post written by logged-in user and friends to post timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,9 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    user_list = current_user.friends.pluck(:id)
+    user_list << current_user.id
+    @timeline_posts ||= Post.all.where(user: user_list || user == current_user).ordered_by_most_recent.includes(:user)
   end
 
   def post_params
