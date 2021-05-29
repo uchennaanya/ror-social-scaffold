cb43580e77f068f42c22380e725f01341b7388a6
uchenna <uchennaongit@gmail.com>
2021-05-29 01:26:29 +0100

    Fix the required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,33 +17,16 @@ class User < ApplicationRecord
   has_many :sent_requests, class_name: 'Friendship', dependent: :destroy, foreign_key: :user_id
   has_many :received_requests, class_name: 'Friendship', dependent: :destroy, foreign_key: :friend_id
 
+
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
   def friends_and_own_posts
     Post.where(user: (friends.to_a << self))
   end
 
-  # def friends
-  #   friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-  #   friends_array.compact
-  # end
-
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  def friend_requests
-    Friendship.where(friend_id: id, confirmed: false)
-  end
-
-  def pending_requests
-    friend_requests.map(&:user).compact
-  end
-
-  def pending_friendship(user)
-    friend_requests.find { |friendship| friendship if friendship.user == user }
-  end
-
-  def pending_friendship?(user)
-    !pending_friendship(user).nil?
   end
 
   def confirm_friend(user)
